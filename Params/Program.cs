b9a8dc54d10e013285b0c46d591a6905ff36bac6
Gustavo Miguel De Barros Carranço <GutMig1112@outlook.com>
2023-04-16 04:23:45 -0300

    Add project files.

---
+++ Params/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Params
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = new int[] { 31, 57, 29, 120, 842, -13, -57, 294 };
+            foreach (int value in values) 
+            {
+                Console.Write(value);
+                Console.Write(",");            
+            }
+            Console.WriteLine("");
+            Console.WriteLine(GetMin(values));
+        }
+
+        public static int GetMin(params int[] values) 
+        {
+            int min = int.MaxValue;
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] < min)
+                {
+                    min = values[i];
+                }
+            }
+            return min;
+        }
+    }
+}
